838a1bf474fb7f13003b187ffa5010218289fbd5
Marcel Pinto <p.marcel.augusto@gmail.com>
2020-10-08 10:14:37 -0300

    Challenge complete

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,15 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const totalPrice = products.reduce((acc, product) => acc + product.price * product.quantity, 0);
 
-    return formatValue(0);
+    return formatValue(totalPrice);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalInCart = products.reduce((acc, product) => acc + product.quantity, 0);
 
-    return 0;
+    return totalInCart;
   }, [products]);
 
   return (
